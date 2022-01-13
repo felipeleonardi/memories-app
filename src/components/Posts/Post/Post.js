d3e596453e6e4b709d6548bdf4625c14e17e901a
Felipe Damaro <felipedamaro@hotmail.com>
2022-01-12 21:17:11 -0300

    initial commit

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography,
+} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons-material/MoreHoriz';
+
+import useStyles from './styles';
+
+const Post = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            <h1>POST</h1>
+        </div>
+    )
+}
+
+export default Post
